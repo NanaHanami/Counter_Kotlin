d7290ffd7fc854946399f2293098b096ab0dc89a
Lera Polyakova <nanahanami@yandex.ru>
2021-08-24 21:27:00 +0500

    Counterv1

---
+++ app/src/main/java/com/example/mycounter/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mycounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun add(view: View) {
+        count++
+        txt.text = count.toString()
+
+
+    }
+
+}
